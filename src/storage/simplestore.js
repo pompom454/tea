1cd1f203bbd1688607c6eccde3cfa9c1fa54d04d
pompom454 <210212892+pompom454@users.noreply.github.com>
2025-12-09 09:26:44 -0700

    Update simplestore.js
    bc optimizations are good 😄👍

--- src/storage/simplestore.js
+++ src/storage/simplestore.js
@@ -1,52 +1,41 @@
 /***********************************************************************************************************************
 
-	storage/simplestore.js
+    storage/simplestore.js
 
-	Copyright © 2013–2025 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
-	Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
+    Copyright © 2013–2025 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
+    Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
 
 ***********************************************************************************************************************/
 
-var SimpleStore = (() => { // eslint-disable-line no-unused-vars, no-var
-	// In-order list of database adapters.
-	const adapters = [];
+var SimpleStore = (() => {
+    const adapters = [];
+    let initialized = null;
 
-	// The initialized adapter.
-	let initialized = null;
+    function create(storageId, persistent) {
+        if (initialized) {
+            return initialized.create(storageId, persistent);
+        }
 
+        for (const adapter of adapters) {
+            try {
+                if (typeof adapter.init !== "function" || typeof adapter.create !== "function") {
+                    throw new TypeError("Adapter missing required methods");
+                }
+                if (adapter.init(storageId, persistent)) {
+                    initialized = adapter;
+                    return adapter.create(storageId, persistent);
+                }
+            }
+            catch (ex) {
+                console.warn(`SimpleStore: adapter "${adapter.name || '[unnamed]'}" failed to init:`, ex);
+            }
+        }
 
-	/*******************************************************************************
-		SimpleStore Functions.
-	*******************************************************************************/
+        throw new Error('No valid storage adapters found');
+    }
 
-	function create(storageId, persistent) {
-		if (initialized) {
-			return initialized.create(storageId, persistent);
-		}
-
-		// Return the first adapter which successfully initializes, elsewise throw an exception.
-		for (let i = 0; i < adapters.length; ++i) {
-			if (adapters[i].init(storageId, persistent)) {
-				initialized = adapters[i];
-				return initialized.create(storageId, persistent);
-			}
-		}
-
-		throw new Error('No valid storage adapters found');
-	}
-
-
-	/*******************************************************************************
-		Object Exports.
-	*******************************************************************************/
-
-	return Object.preventExtensions(Object.create(null, {
-		// Adapters List.
-		//
-		// QUESTION: Should this be a getter, rather than being exported directly?
-		adapters : { value : adapters },
-
-		// Core Functions.
-		create : { value : create }
-	}));
+    return Object.freeze({
+        adapters,
+        create
+    });
 })();
